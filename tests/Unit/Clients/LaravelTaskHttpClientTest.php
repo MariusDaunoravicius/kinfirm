f6bbf8863f7a92885be53efc355723b0b1ff15ab
mariusdaunoravicius <marius.daunoravicius@iterato.lt>
2023-04-20 17:36:17 +0300

    Add tests for DistributorClient

---
+++ tests/Unit/Clients/LaravelTaskHttpClientTest.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Clients;
+
+use App\Clients\DistributorHttpClient;
+use App\DTO\ProductDTO;
+use App\Factories\ProductDTOFactory;
+use Illuminate\Support\Facades\Http;
+use Tests\TestCase;
+
+class LaravelTaskHttpClientTest extends TestCase
+{
+    public function test_fetch_products(): void
+    {
+        $product = [
+            'sku' => fake()->word,
+            'description' => fake()->word,
+            'size' => fake()->word,
+            'photo' => fake()->imageUrl,
+            'tags' => [
+                ['title' => fake()->word],
+            ],
+            'updatedAt' => fake()->date,
+        ];
+
+        Http::fake([
+            'https://kinfirm.com/app/uploads/laravel-task/products.json' => Http::response([$product]),
+        ]);
+
+        $productDTOMock = $this->createMock(ProductDTO::class);
+
+        $productDTOFactoryMock = $this->createMock(ProductDTOFactory::class);
+        $productDTOFactoryMock
+            ->expects(self::once())
+            ->method('createFromArray')
+            ->with($product)
+            ->willReturn($productDTOMock);
+
+        $products = (new DistributorHttpClient($productDTOFactoryMock))->fetchProducts();
+
+        self::assertEquals(collect([$productDTOMock]), $products);
+    }
+}
